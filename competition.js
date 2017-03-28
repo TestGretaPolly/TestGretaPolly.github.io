64f3b7cfe5cf0ebf387a72b4ff12fca3af70e64b
gretabeccarello <gretabeccarello@ymail.com>
2017-03-28 16:27:05 +0100

    Add files via upload
    Make a wish! : )
    Well done to us

--- competition.js
+++ competition.js
@@ -1,6 +1,8 @@
 function showInput() {
         document.getElementById('display').innerHTML = 
                     document.getElementById("user_input").value;
+        document.getElementById('display2').innerHTML = 
+                    document.getElementById("user_input").value;
     }
 
 /* When the user clicks on the button, 
@@ -11,19 +13,19 @@ function myFunction() {
 
 // Close the dropdown menu if the user clicks outside of it
 // Greta I cannot get this to display the seleccte dropdown once clicked
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+//window.onclick = function(event) {
+  //if (!event.target.matches('.dropbtn')) {
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+    //var dropdowns = document.getElementsByClassName("dropdown-content");
+    //var i;
+    //for (i = 0; i < dropdowns.length; i++) {
+      //var openDropdown = dropdowns[i];
+      //if (openDropdown.classList.contains('show')) {
+        //openDropdown.classList.remove('show');
+      //}
+    //}
+  //}
+//}
 
 function showHover(img) {
    if(img) {
